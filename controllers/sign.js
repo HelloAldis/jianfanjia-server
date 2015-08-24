f2c8c551b08ed34628d348ae58be699401497eb9
KindAzrael <307665930@qq.com>
2015-08-24 22:47:19 +0800

    update code

--- controllers/sign.js
+++ controllers/sign.js
@@ -5,10 +5,3 @@ var VerifyCode = require('../proxy').VerifyCode;
 var Designer = require('../proxy').Designer;
 var tools = require('../common/tools');
 var authMiddleWare = require('../middlewares/auth');
-
-// sign out
-exports.signout = function (req, res, next) {
-  req.session.destroy();
-  res.clearCookie(config.auth_cookie_name, { path: '/' });
-  res.redirect('/');
-};
