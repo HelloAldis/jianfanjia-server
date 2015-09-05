bec8926abe721a29101fb8a19b95b49ca8a71ee4
jiayisheji <690405541@qq.com>
2015-09-05 14:19:46 +0800

    好

--- public/static/js/user/design_info.js
+++ public/static/js/user/design_info.js
@@ -393,5 +393,64 @@ $(function(){
 		});
 		return false;
 	});
+	//获取HTML5特性支持情况
+	//图片上传
+	var checkSupport = function(){
+		var input = document.createElement('input');
+		var fileSupport = !!(window.File && window.FileList);
+		var xhr = new XMLHttpRequest();
+		var fd = !!window.FormData;
+		return 'multiple' in input && fileSupport && 'onprogress' in xhr && 'upload' in xhr && fd ? 'html5' : 'flash';
+	};
+	var uploaderUrl = RootUrl+'api/v1/image/upload';
+	if(checkSupport() === "html5"){
+		$('#upload').Huploadify({
+			auto:true,
+			fileTypeExts:'*.jpg;*.png',
+			multi:true,
+			formData:{key:123456,key2:'vvvv'},
+			fileSizeLimit:1024,
+			showUploadedPercent:true,//是否实时显示上传的百分比，如20%
+			showUploadedSize:true,
+			removeTimeout:1,
+			fileObjName:'Filedata',
+			buttonText : "",
+			uploader:uploaderUrl,
+			onUploadComplete:function(file, data, response){
+				callbackImg(data)
+			},
+			onDelete:function(file){
+				console.log('删除的文件：'+file);
+				console.log(file);
+			}
+		});
+	}else{
+		$('#fileToUpload').uploadify({
+	    	'auto'     : true,//关闭自动上传
+	    	'removeTimeout' : 1,//文件队列上传完成1秒后删除
+	        'swf'      : 'uploadify.swf',
+	        'uploader' : RootUrl+'api/v1/image/upload',
+	        'method'   : 'post',//方法，服务端可以用$_POST数组获取数据
+			'buttonText' : '',//设置按钮文本
+	        'multi'    : true,//允许同时上传多张图片
+	        'uploadLimit' : 10,//一次最多只允许上传10张图片
+	        'width' : 250,
+	        'height' : 250,
+	        'fileTypeDesc' : 'Image Files',//只允许上传图像
+	        'fileTypeExts' : '*.gif; *.jpg; *.png',//限制允许上传的图片后缀
+	        'fileSizeLimit' : '1024KB',//限制上传的图片不得超过200KB 
+	        'onUploadSuccess' : function(file, data, response){//每次成功上传后执行的回调函数，从服务端返回数据到前端
+	            callbackImg(data)
+	        },
+	        'onQueueComplete' : function(queueData) {//上传队列全部完成后执行的回调函数
+	           // if(img_id_upload.length>0)
+	           // alert('成功上传的文件有：'+encodeURIComponent(img_id_upload));
+	        }
+	    });
+	}
+	function callbackImg(arr){
+		var data = $.parseJSON(arr)
+		$('#userHead').attr('src',RootUrl+'api/v1/image/'+data.data).data('img',data.data);
+	}
 
 })
---
+++ public/static/js/user/favorites.js
@@ -0,0 +1,83 @@
+$(function(){
+	var $list = $('#favorites-list'); 
+	function loadList(){
+		var url = RootUrl+'api/v1/favorite/product';
+		$.ajax({
+			url:url,
+			type: 'GET',
+			contentType : 'application/json; charset=utf-8',
+			dataType: 'json',
+			success: function(res){
+				var data = res['data']['favorite_product'];
+				if(data.length){
+					for (var i = 0,len = data.length; i < len; i++){
+						shareData(data[i])
+					};
+				}else{
+					$list.html('暂无收藏作品')
+				}
+		   	}
+		});
+		return false;
+	}
+	function shareData(data){
+		var url = RootUrl+'api/v1/product/'+data;
+		$.ajax({
+			url:url,
+			type: 'GET',
+			contentType : 'application/json; charset=utf-8',
+			dataType: 'json',
+			success: function(res){
+				createList(res['data'],data)
+		   	}
+		});
+		return false;
+	}
+	function createList(data,id){
+		var sLi = '';
+		if(data == null){
+			sLi = '<li data-uid="'+id+'">'
+						+'<div class="pic"><img alt="" src="../../img/index/index-live-01.jpg"></div>'
+						+'<div class="txt">'
+							+'<h4>很遗憾</h4>'
+							+'<div class="desc"></div>'
+							+'<p>您收藏的作品已被作品无情的删除，点击取消收藏</p>'
+						+'</div>'
+						+'<a href="javascript:;" class="btn cancel">取消收藏</a>'
+					+'</li>';
+		}else{
+			sLi += '<li>'
+						+'<div class="pic"><img alt="'+data.cell+'" src="'+RootUrl+'api/v1/image/'+data.images[0].imageid+'"></div>'
+						+'<div class="txt">'
+							+'<h4>'+data.cell+'</h4>'
+							+'<div class="desc">'
+								+'<span>'+data.house_area+'m&sup2;</span><span>'+globalData.house_type[data.house_type]+'</span><span>'+globalData.dec_style[data.dec_style]+'</span>'
+							+'</div>'
+							+'<p>'+ellipsisStr(data.description,80)+'</p>'
+						+'</div>'
+						+'<a href="../design/detail.html?'+data._id+'" class="btn">查看详情</a>'
+					+'</li>'
+		}
+		$list.append(sLi);
+	};
+	$list.delegate('.cancel','click',function(ev){
+		ev.preventDefault();
+		var url = RootUrl+'api/v1/favorite/product';
+		var Uid = $(this).closest('li').data('uid');
+		$.ajax({
+			url:url,
+			type: 'DELETE',
+			contentType : 'application/json; charset=utf-8',
+			dataType: 'json',
+			data : JSON.stringify({
+				"_id": Uid
+			}),
+			processData : false,
+			success: function(res){
+				$list.html('');
+				loadList();
+		   	}
+		});
+	});
+	loadList();
+})
