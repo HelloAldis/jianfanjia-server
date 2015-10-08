364cf1054026852b285c5a05c28cf744da09d063
Aldis <307665930@qq.com>
2015-10-08 18:27:14 +0800

    updae sign

--- api/v2/web/sign.js
+++ api/v2/web/sign.js
@@ -1,12 +1,85 @@
 var validator = require('validator');
 var eventproxy = require('eventproxy');
-var Designer = require('../../proxy').Designer;
-var User = require('../../proxy').User;
-var VerifyCode = require('../../proxy').VerifyCode;
-var tools = require('../../common/tools');
-var authMiddleWare = require('../../middlewares/auth');
+var Designer = require('../../../proxy').Designer;
+var User = require('../../../proxy').User;
+var VerifyCode = require('../../../proxy').VerifyCode;
+var tools = require('../../../common/tools');
+var authMiddleWare = require('../../../middlewares/auth');
 var utility = require('utility');
-var sms = require('../../common/sms');
-var ApiUtil = require('../../common/api_util');
-var type = require('../../type');
-var config = require('../../config');
+var sms = require('../../../common/sms');
+var ApiUtil = require('../../../common/api_util');
+var type = require('../../../type');
+var config = require('../../../config');
+var async = require('async');
+
+exports.login = function (req, res, next) {
+  var phone = validator.trim(req.body.phone);
+  var pass = validator.trim(req.body.pass);
+
+  var ep = new eventproxy();
+  ep.fail(next);
+
+  if (!phone || !pass) {
+    return res.sendErrMsg('信息不完整');
+  }
+
+  async.parallel({
+      user: function (callback) {
+        User.findOne({
+          phone: phone
+        }, null, callback);
+      },
+      designer: function (callback) {
+        Designer.findOne({
+          phone: phone
+        }, null, callback);
+      },
+    },
+
+    function (err, result) {
+      if (err) {
+        return next(err);
+      }
+
+      if (result.user && !result.designer) {
+        //业主登录
+        var passhash = result.user.pass;
+        tools.bcompare(pass, passhash, ep.done(function (bool) {
+          if (!bool) {
+            return res.sendErrMsg('用户名或密码错误');
+          }
+
+          // store session cookie
+          authMiddleWare.gen_session(result.user, type.role_user, req,
+            res);
+
+          var data = {};
+          data.url = 'user url';
+          res.sendData(data);
+        }));
+      } else if (!result.user && result.designer) {
+        //设计师登录
+        var passhash = result.designer.pass;
+        tools.bcompare(pass, passhash, ep.done(function (bool) {
+          if (!bool) {
+            return res.sendErrMsg('用户名或密码错误');
+          }
+
+          // store session cookie
+          authMiddleWare.gen_session(result.designer, type.role_designer,
+            req, res);
+
+          var data = {};
+          if (result.designer.agreee_license === type.designer_agree_type_new) {
+            data.url = 'agree license url';
+          } else {
+            data.url = 'designer url';
+          }
+
+          res.sendData(data);
+        }));
+      } else {
+        return res.sendErrMsg('用户名或密码错误');
+      }
+    });
+}
