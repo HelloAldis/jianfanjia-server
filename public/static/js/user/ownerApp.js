daa91d6c6192052436c30673e5c83e9d81ae69a0
jiayisheji <690405541@qq.com>
2015-10-14 18:08:45 +0800

    解决版本冲突

--- public/static/js/user/ownerApp.js
+++ public/static/js/user/ownerApp.js
@@ -8,68 +8,27 @@
             $stateProvider
                 .state('index', {
                     url: '/index',
-<<<<<<< HEAD
-<<<<<<< HEAD
-                    templateUrl: RootURl+'index.html'
-=======
                     templateUrl: RootURl+'index.html',
                     controller : 'indexCtrl'
->>>>>>> d4aa26a3f267cb3169f6a57749440d26a4564c3d
-=======
-                    templateUrl: RootURl+'index.html',
-                    controller : 'indexCtrl'
->>>>>>> 10bb1567a9c920511a1dfe40d1671f1315119a32
                 })
                 .state('infor', {
                     url: '/infor',
                     templateUrl: RootURl+'infor.html',
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
                     controller : 'inforCtrl'
->>>>>>> d4aa26a3f267cb3169f6a57749440d26a4564c3d
-=======
-                    controller : 'inforCtrl'
->>>>>>> 10bb1567a9c920511a1dfe40d1671f1315119a32
                 })
                 .state('release', {
                     url: '/release',
                     templateUrl: RootURl+'release.html',
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
->>>>>>> 10bb1567a9c920511a1dfe40d1671f1315119a32
                     controller : 'releaseCtrl'
                 })
                 .state('revise', {
                     url: '/revise/:id',
                     templateUrl: RootURl+'revise.html',
                     controller : 'releaseCtrl'
-<<<<<<< HEAD
->>>>>>> d4aa26a3f267cb3169f6a57749440d26a4564c3d
-=======
->>>>>>> 10bb1567a9c920511a1dfe40d1671f1315119a32
                 })
                 .state('requirementList', {
                     url: '/requirementList',
                     templateUrl: RootURl+'requirementList.html',
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-                    controller : 'requirementListCtrl'
->>>>>>> 10bb1567a9c920511a1dfe40d1671f1315119a32
-                })
-                .state('requirement', {
-                    url: '/requirement/:id',
-                    templateUrl: RootURl+'requirement.html',
-                    controller: function($scope,$location){
-                        $scope.data = [
-                            {
-                                url : "requirement.detail",
-<<<<<<< HEAD
-                                name : "需求详情"
-=======
                     controller : 'requirementListCtrl'
                 })
                 .state('requirement', {
@@ -80,10 +39,6 @@
                             {
                                 url : "requirement.detail",
                                 name : "需求描述"
->>>>>>> d4aa26a3f267cb3169f6a57749440d26a4564c3d
-=======
-                                name : "需求描述"
->>>>>>> 10bb1567a9c920511a1dfe40d1671f1315119a32
                             },
                             {
                                 url : "requirement.booking",
@@ -114,12 +69,7 @@
                         $scope.abcd = abc($location.$$url.split("/")[2])
                         $scope.b = function(c){
                             $scope.abcd = c
-<<<<<<< HEAD
-<<<<<<< HEAD
-                        }  
-=======
                         } 
->>>>>>> 10bb1567a9c920511a1dfe40d1671f1315119a32
                     }
                 })
                 .state('requirement.detail', {
@@ -146,19 +96,6 @@
                     url: '/score',
                     templateUrl: RootURl+'score.html',
                     controller : 'requirementDetailCtrl'
-                })
-                .state('favorite', {
-                    url: '/favorite',
-                    templateUrl: RootURl+'favorite.html',
-                    controller : 'favoriteProductCtrl'
-                })
-                .state('designer', {
-                    url: '/designer',
-<<<<<<< HEAD
-                    templateUrl: RootURl+'designer.html'
-=======
-                        } 
-                    }
                 })
                 .state('requirement.detail', {
                     url: '/detail',
@@ -194,11 +131,6 @@
                     url: '/designer',
                     templateUrl: RootURl+'designer.html',
                     controller : 'favoriteDesignerCtrl'
->>>>>>> d4aa26a3f267cb3169f6a57749440d26a4564c3d
-=======
-                    templateUrl: RootURl+'designer.html',
-                    controller : 'favoriteDesignerCtrl'
->>>>>>> 10bb1567a9c920511a1dfe40d1671f1315119a32
                 })
         });
     // angular bootstrap
