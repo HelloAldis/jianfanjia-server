9d01411359c15e4fa7700a1243eedf472a7f7aad
Aldis <307665930@qq.com>
2015-12-14 15:24:53 +0800

    add update script

---
+++ db_script/2_0_to_2_1/update_share.js
@@ -0,0 +1,24 @@
+var validator = require('validator');
+var eventproxy = require('eventproxy');
+var Share = require('../../proxy').Share;
+var tools = require('../../common/tools');
+var _ = require('lodash');
+var config = require('../../apiconfig');
+var async = require('async');
+var ApiUtil = require('../../common/api_util');
+var mongoose = require('mongoose');
+var ObjectId = mongoose.Types.ObjectId;
+var type = require('../../type');
+
+Share.find({}, null, null, function (err, shares) {
+  shares.forEach(function (share) {
+    share.process.forEach(function (process) {
+      process.name = (process.name + 1) + '';
+    });
+    share.save(function () {
+      console.log('save to ' + JSON.stringify(share));
+    });
+  });
+});
+
+console.log('over');
