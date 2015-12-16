17f16dfce95c4c570b0806567b0e0130ddb22828
Aldis <307665930@qq.com>
2015-12-16 14:57:17 +0800

    add log for gt

--- getui/gt.js
+++ getui/gt.js
@@ -114,8 +114,8 @@ exports.pushMessageToUser = function (userid, playload) {
   var target = buildTarget(config.gt_user_APPID, userid);
   var message = buildMessage(config.gt_user_APPID, config.gt_user_APPKEY,
     playload);
-  console.log(JSON.strinify(message));
-  console.log(JSON.strinify(target));
+  console.log(JSON.stringify(message));
+  console.log(JSON.stringify(target));
   gt_user.pushMessageToSingle(message, target, function (err, res) {
     console.log('push err = ' + err);
     console.log(res);
@@ -136,8 +136,8 @@ exports.pushMessageToDesigner = function (userid, playload) {
   var target = buildTarget(config.gt_designer_APPID, userid);
   var message = buildMessage(config.gt_designer_APPID, config.gt_designer_APPKEY,
     playload);
-  console.log(JSON.strinify(message));
-  console.log(JSON.strinify(target));
+  console.log(JSON.stringify(message));
+  console.log(JSON.stringify(target));
   gt_designer.pushMessageToSingle(message, target, function (err, res) {
     console.log('push err = ' + err);
     console.log(res);
