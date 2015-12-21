9dffb446eecfc7e0cfb04e004397cf52ec78f5f1
Aldis <307665930@qq.com>
2015-12-21 18:16:46 +0800

    refined the home page for dec article

--- api/v2/web/dec_strategy.js
+++ api/v2/web/dec_strategy.js
@@ -9,29 +9,100 @@ var ApiUtil = require('../../../common/api_util');
 var type = require('../../../type');
 var limit = require('../../../middlewares/limit');
 
-exports.dec_strategy_homepage = function (req, res, next) {
-  var _id = req.params._id;
-  var ep = eventproxy();
-  ep.fail(next);
-
-  DecStrategy.findOne({
-    _id: _id
-  }, null, ep.done(function (dec_strategy) {
-    res.render('dec_strategy', {
-      dec_strategy: dec_strategy,
-    });
-
-    limit.perwhatperdaydo('dec_strategy_homepage', req.ip + _id,
-      1,
-      function () {
-        DecStrategy.incOne({
-          _id: _id
-        }, {
-          view_count: 1
-        });
-      });
-  }));
-}
+// exports.dec_strategy_homepage = function (req, res, next) {
+//   var _id = req.params._id;
+//   var ep = eventproxy();
+//   ep.fail(next);
+//
+//   if (!_id) {
+//     return next(); 
+//   }
+//
+//   DecStrategy.findOne({
+//     _id: _id
+//   }, null, ep.done(function (dec_strategy) {
+//     if (dec_strategy) {
+//       async.parallel({
+//         previous_article: function (callback) {
+//           DecStrategy.find({
+//             articletype: dec_strategy.dec_strategy,
+//             status: type.article_status_public,
+//             create_at: {
+//               $lt: dec_strategy.create_at
+//             },
+//           }, {
+//             title: 1
+//           }, {
+//             skip: 0,
+//             limit: 1,
+//           }, callback);
+//         },
+//         next_article: function (callback) {
+//           DecStrategy.find({
+//             articletype: dec_strategy.dec_strategy,
+//             status: type.article_status_public,
+//             create_at: {
+//               $gt: dec_strategy.create_at
+//             },
+//           }, {
+//             title: 1
+//           }, {
+//             skip: 0,
+//             limit: 1,
+//           }, callback);
+//         },
+//         associate_articles: function (callback) {
+//           var query = {};
+//           keywords = dec_strategy.keywords.split(/,|，/);
+//           _.remove(keywords, function (k) {
+//             return k.trim().length === 0;
+//           });
+//           if (keywords.length > 0) {
+//             var regex = new RegExp(keywords.join('|'), 'i');
+//             query.keywords = regex;
+//           }
+//           query.status = type.article_status_public;
+//           // query.articletype = articletype;
+//           query._id = {
+//             $ne: _id
+//           };
+//
+//           DecStrategy.paginate(query, {
+//             title: 1,
+//             cover_imageid: 1,
+//             articletype: 1,
+//           }, {
+//             sort: {
+//               create_at: -1,
+//             },
+//             skip: 0,
+//             limit: 4,
+//           }, callback);
+//         },
+//       }, ep.done(function (result) {
+//         res.render('dec_strategy', {
+//           dec_strategy: dec_strategy,
+//           previous_article: result.previous_article,
+//           next_article: result.next_article,
+//           associate_articles: result.associate_articles,
+//         });
+//       }));
+//
+//
+//       limit.perwhatperdaydo('dec_strategy_homepage', req.ip + _id,
+//         1,
+//         function () {
+//           DecStrategy.incOne({
+//             _id: _id
+//           }, {
+//             view_count: 1
+//           });
+//         });
+//     } else {
+//       next();
+//     }
+//   }));
+// }
 
 exports.top_articles = function (req, res, next) {
   var limit = req.body.limit || 5;
--- web_router.js
+++ web_router.js
@@ -2,15 +2,17 @@ var express = require('express');
 var config = require('./apiconfig');
 var sign = require('./controllers/sign');
 var site = require('./controllers/site');
+var dec_strategy = require('./controllers/dec_strategy');
 
 var router = express.Router();
 
 // home page
-router.get('/', site.index);
+// router.get('/', site.index);
 router.get('/tpl/user/', site.homePage);
 router.get('/download/user/app', site.download_user_app);
 router.get('/download/user/apk', site.download_user_apk);
 // router.post('/signup', sign.signup);
 // router.post('/login', sign.login);
+router.get('/tpl/article/detail.html', dec_strategy.dec_strategy_homepage);
 
 module.exports = router;
