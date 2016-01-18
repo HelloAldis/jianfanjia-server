a1310a76e10d5b2b0d66f11fe64e942282ddb972
Aldis <307665930@qq.com>
2016-01-18 15:23:07 +0800

    add wechat wenjuan

--- web_router.js
+++ web_router.js
@@ -2,6 +2,7 @@ var express = require('express');
 var config = require('./apiconfig');
 var sign = require('./controllers/sign');
 var site = require('./controllers/site');
+var wechat = require('./controllers/wechat');
 var dec_strategy = require('./controllers/dec_strategy');
 
 var router = express.Router();
@@ -17,4 +18,5 @@ router.get('/download/designer/apk', site.download_designer_apk);
 router.get('/tpl/article/detail.html', dec_strategy.dec_strategy_homepage);
 router.get('/wechat/user_login_callback', sign.wechat_user_login_callback);
 router.get('/wechat/user_login', sign.wechat_user_login);
+router.get('/wechat/user_wenjuan/:wenjuanid', wechat.user_wenjuan);
 module.exports = router;
