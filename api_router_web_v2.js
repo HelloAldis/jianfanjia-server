be83aca23c51fafc546f28b3f122054f2d485c1e
Aldis <307665930@qq.com>
2016-02-17 16:45:58 +0800

    add start process for web

--- api_router_web_v2.js
+++ api_router_web_v2.js
@@ -1,4 +1,3 @@
-var express = require('express');
 var tempUserApi = require('./api/v2/web/temp_user');
 var sign = require('./api/v2/web/sign');
 var image = require('./api/v2/web/image');
@@ -17,10 +16,14 @@ var wechat = require('./api/v2/web/wechat');
 var dec_strategy = require('./api/v2/web/dec_strategy');
 var beautiful_image = require('./api/v2/web/beautiful_image');
 var answer = require('./api/v2/web/answer');
+
+var processApp = require('./api/v2/app/process');
+
 var config = require('./apiconfig');
 var auth = require('./middlewares/auth');
 var limit = require('./middlewares/limit');
 
+var express = require('express');
 var router = express.Router();
 
 var multer = require('multer')
@@ -106,6 +109,7 @@ router.post('/user_evaluate_designer', auth.userRequired, user.user_evaluate_des
 router.post('/user_statistic_info', auth.userRequired, user.user_statistic_info); //业主获取自己统计信息
 router.post('/user_bind_wechat', auth.userRequired, user.user_bind_wechat); //业主绑定微信
 router.post('/user_bind_phone', auth.userRequired, user.user_bind_phone); //业主绑定手机号
+router.post('/user/process', auth.userRequired, processApp.start); //开启装修流程
 
 //设计师独有功能
 router.post('/designer/agree', auth.designerRequired, designer.agree); //同意条款
