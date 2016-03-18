f8fd209f47d4f347a016f47d27dfe48d9b960ec6
Aldis <307665930@qq.com>
2016-03-18 18:25:31 +0800

    rename script

---
+++ upgrade_script/2_3_to_2_4/upgrade_node.sh
@@ -0,0 +1,8 @@
+#don't run it directly, it just cmd backup.
+
+nvm install v4.4.0
+npm update npm -g
+npm install -g cnpm --registry=https://registry.npm.taobao.org
+npm install -g node-gyp
+npm install -g pm2
+npm rb
