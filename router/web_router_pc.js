4ccfc16546a2f163454fd5eda3c507875d9143a0
Aldis <307665930@qq.com>
2016-05-05 11:13:51 +0800

    fix the ejs return

--- router/web_router_pc.js
+++ router/web_router_pc.js
@@ -19,7 +19,7 @@ router.get('/download/user/apk', site.download_user_apk);
 router.get('/download/designer/apk', site.download_designer_apk);
 // router.post('/signup', sign.signup);
 // router.post('/login', sign.login);
-router.get('/tpl/article/detail.html', dec_strategy.dec_strategy_homepage);
+router.get('/tpl/article/detail.html', response_util, dec_strategy.dec_strategy_homepage);
 router.get('/wechat/user_login_callback', sign.wechat_user_login_callback);
 router.get('/wechat/user_login', sign.wechat_user_login);
 router.get('/wechat/user_wenjuan/:wenjuanid', wechat.user_wenjuan);
