6147dd582f78c6a3fb2525b4c3a452ae2b7e6038
Aldis <307665930@qq.com>
2016-05-26 15:17:51 +0800

    fix the app.js

--- app.js
+++ app.js
@@ -15,9 +15,9 @@ let main_app = express();
 main_app.use(vhost('jianfanjia.com', function (req, res, next) {
   res.redirect(301, 'http://www.jianfanjia.com' + req.url);
 }));
-// main_app.use(vhost(config.m_web_domain_regex, require('./app_mobile')));
-main_app.use(vhost(config.www_web_domain_regex, require('./app_pc')));
 main_app.use(vhost(config.admin_web_domain_regex, require('./app_admin')));
+main_app.use(vhost(config.m_web_domain_regex, require('./app_mobile')));
+main_app.use(vhost(config.www_web_domain_regex, require('./app_pc')));
 
 main_app.listen(config.port, function () {
   logger.info('Jianfanjia listening on port %s', config.port);
