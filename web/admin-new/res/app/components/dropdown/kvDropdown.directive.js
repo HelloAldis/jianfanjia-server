5e769ed5ce8eb6aec71a2b1f1a1411c1dab80fe0
karosLi <karosli1314@gmail.com>
2016-06-13 15:19:09 +0800

    refine

--- web/admin-new/res/app/components/dropdown/kvDropdown.directive.js
+++ web/admin-new/res/app/components/dropdown/kvDropdown.directive.js
@@ -17,15 +17,17 @@
         keyval: '@',
         express: '@',
       },
-      restrict: 'EA',
+      restrict: 'E',
       templateUrl: 'app/components/dropdown/kvDropdown.html',
       controller: function ($scope, $element, $attrs, $transclude) {
         var keyValname = $scope.keyval.split(/:/);
         $scope.kvKeyName = keyValname[0];
         $scope.kvValName = keyValname[1];
-        // console.log('$scope.kvKeyName === ' + $scope.kvKeyName);
-        $scope.kvCurOption = $scope.opts.find(function (option) {
-          return option[$scope.kvKeyName] === $scope.curkey;
+
+        $scope.$watch('curkey', function (newVal, oldVal) {
+          $scope.kvCurOption = $scope.opts.find(function (option) {
+            return option[$scope.kvKeyName] == newVal;
+          });
         });
 
         $scope.selectAOption = function(choice){
