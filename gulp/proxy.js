072d0f7a956ece880a53e28e176a83d0d0d11ae6
Aldis <307665930@qq.com>
2016-07-05 10:25:37 +0800

    add gulp file for proxy

---
+++ gulp/proxy.js
@@ -0,0 +1,99 @@
+'use strict'
+/**
+
+gulp proxy -p  启动代理pc网站 http://localhost:9000
+gulp proxy -a  启动代理admin网站 http://localhost:9001
+gulp proxy -m  启动代理移动端网站 http://localhost:9002
+
+*/
+
+const gulp = require('gulp');
+const connect = require('gulp-connect');
+const url = require('url');
+const watch = require('gulp-watch');
+const proxy = require('proxy-middleware');
+const modRewrite = require('connect-modrewrite');
+const minimist = require('minimist');
+
+//替换js
+//<!-- build:js scripts/scripts.js -->
+//<script src="scripts/app.js"></script>
+//<script src="scripts/controllers/main.js"></script>
+//<!-- endbuild -->
+const jsRegExp = /(<!--\s*build\:js)([\s\S]*)(endbuild\s*-->)/g;
+
+//替换css
+//<!-- build:css css/css.js -->
+//<link href="css/css.js"></link>
+//<link href="css/controllers/css.js"></link>
+//<!-- endbuild -->
+const cssRegExp = /(<!--\s*build\:css)([\s\S]*)(endbuild\s*-->)/g
+
+
+const pc_web_port = 9000;
+const pc_web_root = './web/pc/res'
+const admin_web_port = 9001;
+const admin_web_root = './web/admin-new/res'
+const mobile_web_port = 9002;
+const mobile_web_root = './web/mobile/res'
+
+
+function getRoot(argv) {
+  if (argv.m) {
+    return mobile_web_root;
+  } else if (argv.a) {
+    return admin_web_root;
+  } else {
+    return pc_web_root;
+  }
+}
+
+function getPort(argv) {
+  if (argv.m) {
+    return mobile_web_port;
+  } else if (argv.a) {
+    return admin_web_port;
+  } else {
+    return pc_web_port;
+  }
+}
+
+gulp.task('connect', function () { //配置代理
+  const argv = minimist(process.argv.slice(3));
+  const port = getPort(argv);
+  const root = getRoot(argv);
+  connect.server({
+    root: root,
+    host: 'localhost',
+    port: port,
+    livereload: true,
+    middleware: function (connect, opt) {
+      return [
+        (function () {
+          var options = url.parse('http://dev.jianfanjia.com/api');
+          options.route = '/api';
+          options.cookieRewrite = 'dev.jianfanjia.com';
+          return proxy(options);
+        })(),
+        modRewrite([
+          '^/api(.*)$ http://dev.jianfanjia.com/api$1 [P]'
+        ])
+      ];
+    }
+  });
+});
+gulp.task('watch-proxy', function () { //监听变化
+  const argv = minimist(process.argv.slice(3));
+  const root = getRoot(argv);
+  const html = root + '/**/*.html';
+  const css = root + '/**/*.css';
+  const js = root + '/**/*.js';
+  const jpeg = root + '/**/*.jpeg';
+  const jpg = root + '/**/*.jpg';
+  const png = root + '/**/*.png';
+
+  gulp.src([html, css, js])
+    .pipe(watch([html, css, js]))
+    .pipe(connect.reload());
+});
+gulp.task('proxy', ['connect', 'watch-proxy']);
---
+++ gulp/release.js
@@ -0,0 +1,123 @@
+'use strict'
+
+/**
+  Release build as command  gulp release (-a|-b|-c)
+*/
+
+const gulp = require('gulp');
+const minimist = require('minimist')
+const runSequence = require('run-sequence');
+const bump = require('gulp-bump');
+const gutil = require('gulp-util');
+const git = require('gulp-git');
+const fs = require('fs');
+const concat = require('gulp-concat');
+const through2 = require('through2')
+
+
+// -------------------------------- Common Function ----------------------------------------
+function getPackageJsonVersion() {
+  // 这里我们直接解析 json 文件而不是使用 require，这是因为 require 会缓存多次调用，这会导致版本号不会被更新掉
+  return JSON.parse(fs.readFileSync('./package.json', 'utf8')).version;
+};
+
+// -------------------------------- End Common Function ----------------------------------------
+
+gulp.task('default', function () {
+  console.log('please use command "gulp release [-a|-b|-c]" or "gulp deploy [-p|-t|-d] [-n]"');
+});
+
+gulp.task('code', function () {
+  return gulp.src(['api/**/*.js'])
+    .pipe(concat('code.txt'))
+    .pipe(gulp.dest('./'));
+});
+
+// -------------------------------- Release Function ----------------------------------------
+gulp.task('bump-version', function () {
+  var argv = minimist(process.argv.slice(3));
+  var type = {
+    type: "patch"
+  };
+  if (argv.a) {
+    type = {
+      type: "major"
+    };
+  } else if (argv.b) {
+    type = {
+      type: "minor"
+    };
+  }
+
+  return gulp.src(['./package.json']).pipe(bump(type).on('error', gutil.log)).pipe(gulp.dest('./'));
+});
+
+gulp.task('commit-changes', function () {
+  return gulp.src('.')
+    .pipe(git.commit('[Release] Bumped version number: ' +
+      getPackageJsonVersion(), {
+        args: '-a'
+      }));
+});
+
+gulp.task('push-changes', function (cb) {
+  git.push('origin', 'phase2', cb);
+});
+
+gulp.task('create-new-tag', function (cb) {
+  var version = getPackageJsonVersion();
+  git.tag('build-' + version, 'Created Tag for version: ' + version,
+    function (error) {
+      if (error) {
+        return cb(error);
+      }
+      git.push('origin', 'build-' + version, cb);
+    });
+});
+
+gulp.task('release', function (callback) {
+  runSequence(
+    'bump-version',
+    'commit-changes',
+    'push-changes',
+    'create-new-tag',
+    function (error) {
+      if (error) {
+        console.log(error.message);
+      } else {
+        console.log('RELEASE FINISHED SUCCESSFULLY');
+      }
+      callback(error);
+    });
+});
+// -------------------------------- End Release Function ----------------------------------------
+
+// -------------------------------- Deploy Function ----------------------------------------
+gulp.task('deploy', function (callback) {
+  runSequence(
+    'cp-config',
+    function (error) {
+      if (error) {
+        console.log(error.message);
+      } else {
+        console.log('Deploy FINISHED SUCCESSFULLY');
+      }
+      callback(error);
+    });
+});
+
+gulp.task('cp-config', function () {
+  const argv = minimist(process.argv.slice(3));
+  console.log(argv);
+  var path = './apiconfig.dev.js';
+  if (argv.t) {
+    path = './apiconfig.test.js';
+  } else if (argv.p) {
+    path = './apiconfig.pro.js';
+  }
+
+  console.log('cp ' + path + ' to ./apiconfig.js');
+  gulp.src(path).pipe(gulp.dest('./apiconfig.js'));
+});
+
+// -------------------------------- End Deploy Function ----------------------------------------
