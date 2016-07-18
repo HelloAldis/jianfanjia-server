8baadd5959ce91b62593f76559c93bfd87da4f7c
Aldis <307665930@qq.com>
2016-07-18 15:27:21 +0800

    add new admin api

--- router/api_router_web_v2.js
+++ router/api_router_web_v2.js
@@ -210,5 +210,7 @@ router.post('/admin/search_diary', auth.adminRequired, admin.search_diary); // �
 router.post('/admin/delete_diary', auth.adminRequired, admin.delete_diary); // 删除日记
 router.post('/admin/search_comment', auth.adminRequired, admin.search_comment); // 搜索评论
 router.post('/admin/forbid_comment', auth.adminRequired, admin.forbid_comment); // 屏蔽评论
+router.post('/admin/search_supervisor', auth.adminRequired, admin.search_supervisor); // 搜索监理
+router.post('/admin/assign_supervisor', auth.adminRequired, admin.assign_supervisor); // 指派监理到工地
 
 module.exports = router;
