89430daa845fafb7f7be099c65825266ea6cea34
Aldis <307665930@qq.com>
2016-07-27 17:08:44 +0800

    fix the admin push bugs

--- api/v2/web/admin.js
+++ api/v2/web/admin.js
@@ -1398,10 +1398,10 @@ exports.search_image = function (req, res, next) {
         userid: search_word
       }];
     } else {
-      return res.sendData({
-        images: [],
-        total: 0
-      });
+      search_word = reg_util.reg(tools.trim(search_word), 'i');
+      query['$or'] = [{
+        ip: search_word
+      }];
     }
   }
   let ep = eventproxy();
