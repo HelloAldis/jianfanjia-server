579783e5fbc5222363563aac43661cc325ead8c8
Aldis <307665930@qq.com>
2016-08-15 12:10:52 +0800

    update admin build gulp

--- gulp/admin_dev.js
+++ gulp/admin_dev.js
@@ -9,16 +9,43 @@ gulp devm  启动移动端开发网站 http://localhost:9002
 
 const gulp = require('gulp');
 const util = require('./util');
+const inject = require('gulp-inject');
+const mainBowerFiles = require('main-bower-files');
 
 const admin_web_port = 9001;
-const admin_web_root = './web/admin-new/res';
+const admin_res = './web/admin-new/res';
 
 gulp.task('admin-connect', function () { //配置代理
-  util.proxy(admin_web_root, admin_web_port);
+  util.proxy(admin_res, admin_web_port);
 });
 
 gulp.task('admin-reload', function () { //监听变化
-  return util.reload(admin_web_root);
+  return util.reload(admin_res);
+});
+
+gulp.task('admin-inject', function () {
+  var injectApp = gulp.src([admin_res + '/app/**/*.module.js', admin_res + '/app/**/*.js', '!' + admin_res + '/app/**/templates.js'], {
+    read: false
+  });
+  var injectAppOption = {
+    name: 'app',
+    relative: 'true'
+  };
+
+  var injectVender = gulp.src(mainBowerFiles({
+    paths: admin_res
+  }), {
+    read: false
+  });
+  var injectVenderOption = {
+    name: 'vender',
+    relative: 'true'
+  };
+
+  return gulp.src(admin_res + '/*.html')
+    .pipe(inject(injectApp, injectAppOption))
+    .pipe(inject(injectVender, injectVenderOption))
+    .pipe(gulp.dest(admin_res));
 });
 
 gulp.task('deva', ['admin-connect', 'admin-reload', 'css', 'watch-css']);
--- web/admin-new/res/app/index.controller.js
+++ web/admin-new/res/app/index.controller.js
@@ -1,7 +1,8 @@
 (function () {
   angular.module('JfjAdmin')
     .controller('topCtrl', [
-      '$scope', function ($scope) {
+      '$scope',
+      function ($scope) {
         $scope.$on('$stateChangeSuccess', function (event, current, previous) {
           $scope.title = current.title;
         });
