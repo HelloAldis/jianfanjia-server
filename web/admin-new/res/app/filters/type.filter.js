dd5dfb5b5e7ab4ee5085bc6a9dd91b399ed6ff9d
Aldis <307665930@qq.com>
2016-08-22 15:39:24 +0800

    update temp user list

--- web/admin-new/res/app/filters/type.filter.js
+++ web/admin-new/res/app/filters/type.filter.js
@@ -218,14 +218,6 @@
         }[input];
       };
     })
-    平台
-    * 0. 安卓APP
-    * 1. iOSAPP
-    * 2. PC web
-    * 3. 微信
-    * 4. 管理员
-    * 5. iOS mobile web
-    * 6. Android mobile web
     .filter('platformFilter', function () {
       return function (input) {
         return {
